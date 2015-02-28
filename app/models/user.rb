f48c8f099a896dd8f26e5fc4113240fcf300c035
kudohamu <kudohamu91086@yahoo.co.jp>
2015-02-28 14:50:50 +0900

    ログイン方法としてTwitter認証を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,10 @@
+require "open-uri"
+
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
   
   has_many :records
 
@@ -15,7 +17,28 @@ class User < ActiveRecord::Base
 
   validates_uniqueness_of :uid, scope: [:provider]
 
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+    unless user
+      fileName = File.basename(auth.info.image)
+      filePath = File.join(ApplicationController.get_user_icon_tmp_dir, fileName)
+
+      FileUtils.mkdir_p(ApplicationController.get_user_icon_tmp_dir) unless FileTest.exist?(ApplicationController.get_user_icon_tmp_dir)
+      open(filePath, 'wb') do |output|
+        open(auth.info.image) do |data|
+          output.write(data.read)
+        end
+      end
+      user = User.create!(name: auth.info.nickname, icon: File.open(filePath, "rb"), provider: auth.provider, uid: auth.uid, email: User.create_unique_email, password: Devise.friendly_token[0,20])
+    end
+    user
+  end
+
   def self.create_unique_string
     SecureRandom.uuid
+  end
+
+  def self.create_unique_email
+    User.create_unique_string + "@example.com"
   end
 end
