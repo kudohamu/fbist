8ce8cd94932fb5189de90fc5b67628efc21ecf01
kudohamu <kudohamu91086@yahoo.co.jp>
2015-03-01 13:21:09 +0900

    テーブルのヘッダにソート順表示用の画像を追加

--- app/assets/javascripts/app/records/records_controller.js
+++ app/assets/javascripts/app/records/records_controller.js
@@ -19,22 +19,53 @@ recordsSummaryApp.controller('summaryController', ['$scope', 'JsonData', functio
 recordsSummaryApp.controller('userSummaryController', function($scope, $resource) {
   var res = $resource('/api/account/records/total');
   $scope.total = res.get(function() {
-    $scope.userLoading = true
+    $scope.userLoading = true;
   }, 
   function() {
-    $scope.total = {"record": "-- ", "won": "-- ", "lost": "-- ", "rate": "-- "}
-    $scope.userLoading = true
+    $scope.total = {"record": "-- ", "won": "-- ", "lost": "-- ", "rate": "-- "};
+    $scope.userLoading = true;
   });
 });
 
 recordsSummaryApp.controller('gundamSummaryController', function($scope, $resource) {
   var res = $resource('/api/account/records/summary');
   $scope.records = res.query(function() {
-    $scope.recordsLoading = true
-    $scope.loadingSuccess = true
+    $scope.reverse = false;
+    $scope.predicate = "no";
+    $scope.sort = { "no": true, "total": false, "won": false, "lost": false, "rate": false };
+    $scope.asc = true;
+    $scope.recordsLoading = true;
+    $scope.loadingSuccess = true;
   }, 
   function () {
-    $scope.recordsLoading = true
-    $scope.loadingFailed = true
+    $scope.recordsLoading = true;
+    $scope.loadingFailed = true;
   });
+
+  $scope.thClick = function(pred) {
+    if ($scope.predicate == pred) {
+      $scope.reverse = !$scope.reverse;
+      $scope.asc = !$scope.asc;
+    }else {
+      $scope.predicate = pred;
+      $scope.reverse = false;
+      $scope.asc = true;
+
+      $scope.sort["no"] = false;
+      $scope.sort["total"] = false;
+      $scope.sort["won"] = false;
+      $scope.sort["lost"] = false;
+      $scope.sort["rate"] = false;
+
+      $scope.sort[pred] = true;
+    }
+  };
+
+  $scope.arrowShow = function(pred, sc) {
+    if (sc == "asc") {
+      return ($scope.sort[pred] && $scope.asc);
+    }else {
+      return ($scope.sort[pred] && !($scope.asc));
+    }
+  };
 });
