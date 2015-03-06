2851859e463221cd0e4fdfdfdf9445d503d454f3
kudohamu <kudohamu91086@yahoo.co.jp>
2015-03-07 03:51:55 +0900

    seedデータを変更

--- db/seeds/development/users.rb
+++ db/seeds/development/users.rb
@@ -1,10 +1,40 @@
 User.create(
   id: 1,
-  name: "kudohamu",
+  name: "その他",
   icon: File.open(File.join(Rails.root, "app/assets/images/test.jpg")),
   uid: SecureRandom.uuid,
   provider: "",
-  email: "kudohamu@gmail.com",
+  email: "test@gmail.com",
+  password: "hogehoge",
+  password_confirmation: "hogehoge"
+)
+
+User.create(
+  name: "hogeuser",
+  icon: File.open(File.join(Rails.root, "app/assets/images/test.jpg")),
+  uid: SecureRandom.uuid,
+  provider: "",
+  email: "hoge@gmail.com",
+  password: "hogehoge",
+  password_confirmation: "hogehoge"
+)
+
+User.create(
+  name: "hugauser",
+  icon: File.open(File.join(Rails.root, "app/assets/images/test.jpg")),
+  uid: SecureRandom.uuid,
+  provider: "",
+  email: "huga@gmail.com",
+  password: "hogehoge",
+  password_confirmation: "hogehoge"
+)
+
+User.create(
+  name: "hageuser",
+  icon: File.open(File.join(Rails.root, "app/assets/images/test.jpg")),
+  uid: SecureRandom.uuid,
+  provider: "",
+  email: "hage@gmail.com",
   password: "hogehoge",
   password_confirmation: "hogehoge"
 )
