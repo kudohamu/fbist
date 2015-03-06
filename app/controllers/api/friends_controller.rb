8476c6a36b2c96ea0f17b59de599c741bfff486d
kudohamu <kudohamu91086@yahoo.co.jp>
2015-03-07 03:52:37 +0900

    Api::FriendsControllerのindexアクションで、フレンドの一覧を取得できるよ
    うに

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -2,6 +2,11 @@ class Api::FriendsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
-    
+    @friends = current_user.friends
+
+    if params[:other].present?
+      @friends.unshift(User.find_by_id(1))
+    end
+    render :formats => [:json], :handlers => [:jbuilder]
   end
 end
