fca25cb67a4269d14c4c6942dde5c276002b6429
kudohamu <kudohamu91086@yahoo.co.jp>
2015-03-17 09:55:19 +0900

    共通機能をサービスに切り出し

--- app/assets/javascripts/base_services.js
+++ app/assets/javascripts/base_services.js
@@ -1,6 +1,5 @@
 var appName = "FVistFoundation";
-//var fvistFoundationApp = angular.module(appName);
-var fvistFoundationApp = angular.module(appName, ["ngResource"]);
+var fvistFoundationApp = angular.module(appName, ["ngResource", "ngAnimate"]);
 
 fvistFoundationApp.config(
   ["$httpProvider", function($httpProvider) {
@@ -8,6 +7,89 @@ fvistFoundationApp.config(
   }]
 );
 
-fvistFoundationApp.factory("recordFactory", function($resource) {
-  return $resource("/api/account/records/:id", {id: "@id"});
-});
+//resource周りのfactory
+fvistFoundationApp.factory("recordFactory", ["$resource", function($resource) {
+  return $resource("/api/account/records/:id", {id: "@id"}, {
+    update: {
+      method: "PUT"
+    }
+  });
+}]);
+
+fvistFoundationApp.factory("gundamsFactory", ["$resource", function($resource) {
+  return $resource("/api/gundams");
+}]);
+
+fvistFoundationApp.factory("friendsFactory", ["$resource", function($resource) {
+  return $resource("/api/account/friends");
+}]);
+
+//filter周りの共通処理
+fvistFoundationApp.filter("gundamListFilter", ["$rootScope", "$filter", function($rootScope, $filter) {
+  return function(items, input) {
+    var result = $filter("filter")(items, input);
+    $rootScope.filtedGundams = result;
+    return result;
+  };
+}]);
+
+fvistFoundationApp.filter("friendListFilter", ["$rootScope", "$filter", function($rootScope, $filter) {
+  return function(items, input) {
+    var result = $filter("filter")(items, input);
+    $rootScope.filtedFriends = result;
+    return result;
+  };
+}]);
+
+//共通機能のfactory
+fvistFoundationApp.factory("selectGundamFactory", ["$rootScope", function($rootScope) {
+  return {
+    selectGundam: function(index, record) {
+      if ($rootScope.filtedGundams) {
+        record.gundam.image_path = $rootScope.filtedGundams[index].image_path;
+        record.gundam.name = $rootScope.filtedGundams[index].name;
+        record.gundam.id = $rootScope.filtedGundams[index].id;
+      }else {
+        record.gundam.image_path = $rootScope.gundams[index].image_path;
+        record.gundam.name = $rootScope.gundams[index].name;
+        record.gundam.id = $rootScope.gundams[index].id;
+      }
+    },
+
+    selectGundamClick: function() {
+      $rootScope.gundamListDivShow = !$rootScope.gundamListDivShow;
+    }
+  };
+}]);
+
+fvistFoundationApp.factory("selectFriendFactroy", ["$rootScope", function($rootScope) {
+  return {
+    selectFriend: function(index, record) {
+      if ($rootScope.filtedFriends) {
+        record.friend.image_path = $rootScope.filtedFriends[index].image_path;
+        record.friend.name = $rootScope.filtedFriends[index].name;
+        record.friend.id = $rootScope.filtedFriends[index].id;
+      }else {
+        record.friend.image_path = $rootScope.friend_list[index].image_path;
+        record.friend.name = $rootScope.friend_list[index].name;
+        record.friend.id = $rootScope.friend_list[index].id;
+      }
+    },
+
+    selectFriendClick: function() {
+      $rootScope.friendListDivShow = !$rootScope.friendListDivShow;
+    }
+  };
+}]);
+
+fvistFoundationApp.factory("messageFactory", ["$rootScope", "$timeout", function($rootScope, $timeout) {
+  return {
+    show: function(message) {
+      $rootScope.message = message;
+      $rootScope.messageCSS = "messageShow";
+      $timeout(function() {
+        $rootScope.messageCSS = "";
+      }, 5000);
+    }
+  };
+}]);
--- app/assets/javascripts/records/summary_services.js
+++ app/assets/javascripts/records/summary_services.js
@@ -1,1 +1,7 @@
+fvistFoundationApp.factory("recordTotalFactory", ["$resource", function($resource) {
+  return $resource("/api/account/records/total");
+}]);
 
+fvistFoundationApp.factory("recordSummaryFactory", ["$resource", function($resource) {
+  return $resource("/api/account/records/summary");
+}]);
