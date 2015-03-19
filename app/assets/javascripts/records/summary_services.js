57524ca521877f0f92fb669a541ab6d31af75bb3
kudohamu <kudohamu91086@yahoo.co.jp>
2015-03-19 21:05:03 +0900

    recordTotalFactoryを移動
    records/summary_services.js -> base_services.js

--- app/assets/javascripts/records/summary_services.js
+++ app/assets/javascripts/records/summary_services.js
@@ -1,7 +1,3 @@
-fvistFoundationApp.factory("recordTotalFactory", ["$resource", function($resource) {
-  return $resource("/api/account/records/total");
-}]);
-
 fvistFoundationApp.factory("recordSummaryFactory", ["$resource", function($resource) {
   return $resource("/api/account/records/summary");
 }]);
