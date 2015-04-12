ad1ea468c9aba1aba8d669439bd0695cb8586e5f
kudohamu <kudohamu91086@yahoo.co.jp>
2015-04-12 12:57:31 +0900

    GundamsRankモデルのバリデーションを作成

--- app/models/gundams_rank.rb
+++ app/models/gundams_rank.rb
@@ -1,2 +1,10 @@
 class GundamsRank < ActiveRecord::Base
+
+  belongs_to :rank
+  belongs_to :gundam
+
+  validates_presence_of :rank_id
+  validates_associated :rank
+
+  validates_presence_of :gundam_id
 end
