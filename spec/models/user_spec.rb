439bfe92a156cf27e0e8e34cd833dc3b761ffa33
kudohamu <kudohamu91086@yahoo.co.jp>
2015-04-12 12:22:42 +0900

    Userモデルのテストを作成

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,17 @@ require 'rails_helper'
 
 RSpec.describe User, :type => :model do
   it { should have_many(:records) }
+  it { should have_many(:friendlist_of_from_user).with_foreign_key(:from_user_id) }
+  it { should have_many(:friendlist_of_to_user).with_foreign_key(:to_user_id) }
+  it { should have_many(:friends_of_from_user) }
+  it { should have_many(:friends_of_to_user) }
 
   describe "#name" do
     it { should validate_presence_of(:name) }
     it { should ensure_length_of(:name).is_at_most(20) }
+  end
+
+  describe "#icon" do
+    it { should validate_presence_of(:icon) }
   end
 end
